3c9f0a44b5003c6d8d880712f5b5ccdbe01ace8f
Robert Young <robertayoung2022@gmail.com>
2021-11-22 20:46:59 -0600

    primitive exercise updates

--- primitive.js
+++ primitive.js
@@ -1,7 +1,16 @@
+//number
 console.log(99.9);
+//boolean
 console.log("false");
+//typeof
 console.log(typeof false);
+//string
 console.log('0');
+//number
 console.log(0);
+//boolean
 console.log(true);
+//string
 console.log('true');
+
+//REDO Assignment
