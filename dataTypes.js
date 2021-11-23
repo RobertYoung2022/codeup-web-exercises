5184345a4446cbd19526e2e58570e5fe168226d9
Robert Young <robertayoung2022@gmail.com>
2021-11-22 20:34:45 -0600

    operators exercise

--- dataTypes.js
+++ dataTypes.js
@@ -1,6 +1,85 @@
+// Exercise 1.1
 var a = 1;
 console.log(a);
 var b = a++;
 console.log(b);
 var c = ++a;
 console.log(c);
+
+// var a = 1;
+// console.log(a);
+// var b = a++;
+// console.log(b);
+// var c = b++;
+// console.log(c);
+// var d = c++;
+// console.log(d);
+
+// var a = 1;
+// console.log(a);
+// var b = ++a;
+// console.log(b);
+// var c = ++b;
+// console.log(c);
+// var d = ++c;
+// console.log(d);
+
+
+//Exercise 1.2
+var d = "hello";
+d++;
+console.log(d);
+
+var e = false;
+e++;
+console.log(e);
+
+//Exercise 1.3
+var perplexed;
+perplexed + 2;
+console.log(perplexed);
+
+var perplexed = 4;
+perplexed + 2;
+console.log(perplexed);
+
+//Exercise 1.4
+var price = 2.7;
+price.toFixed(2);
+console.log(price);
+
+//Exercise 1.5
+// var price = "2.7";
+// price.toFixed(2);
+// console.log(price);
+
+//Exercise 1.6
+isNaN(0)
+
+isNaN(1)
+
+isNaN("")
+
+isNaN("string")
+
+isNaN("0")
+
+isNaN("1")
+
+isNaN("3.145")
+
+isNaN(Number.MAX_VALUE)
+
+isNaN(Infinity)
+
+isNaN("true")
+
+isNaN(true)
+
+isNaN("false")
+
+isNaN(false)
+
+console.log(isNaN());
+// to illustrate why the isNaN() function is needed:
+NaN == NaN
--- operators.js
+++ operators.js
@@ -1,17 +1,34 @@
+//12
 console.log('1' + 2);
+//string
 console.log(typeof '1' + 2);
+//2
 console.log(6 % 4);
+//7
 console.log('3 + 4 is '+ (3 + 4));
+//false
 console.log(0 < 0);
+//false
 console.log('false' == false);
+//false
 console.log(true == 'true');
+//true
 console.log(5 >= -5);
+//true
 console.log(!false || false);
+//true
 console.log(true || "42");
+//false
 console.log(!true && !false);
+//false
 console.log(5 < 4 && 1 === 1);
+//true
 console.log(typeof 'codeup' === 'string');
-console.log('codeup');
-console.log();
-console.log();
-console.log();
+//false
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+//true
+console.log(4 >= 0 && 1 !== '1');
+//true
+console.log(6 % 3 === 0);
+//true
+console.log(5 % 2 !== 0);
