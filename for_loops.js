0013c22ded4b7f8795f437882e2fec529533136a
Robert Young <robertayoung2022@gmail.com>
2021-12-02 18:27:57 -0600

    first loop assignment complete

---
+++ for_loops.js
@@ -0,0 +1,57 @@
+"use strict";
+
+
+// 2.
+// Create a function named showMultiplicationTable that accepts a number and
+// console.logs the multiplication table for that number
+// (just multiply by the numbers 1 through 10)
+
+/*function showMultiplicationTable(table) {
+    for(let number = 1; number <= 10; number++) {
+        console.log("Multiplication table for your number is " + table * number);
+    }
+}
+showMultiplicationTable(3);*/
+
+//  3.
+/*
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+function showMult() {
+
+    for(let number = 1; number <= 10; number++) {
+        let here = getRandomIntInclusive(20, 200);
+
+        if(here % 2 == 0) {
+            console.log(here + " The number is even.");
+        } else {
+                console.log(here + " The number is odd.");
+            }
+    }
+}
+showMult();*/
+
+//  4.
+/*for(let a = 1; a <= 9; a++) {
+    let s = '';
+    for(let b = 1; b <= a; b++) {
+        s += a;
+    }
+    console.log(s);
+}*/
+
+// Create a for loop that uses console.log to create the output shown below
+
+//  5.
+/*
+for(let i = 100; i >= 1; i--) {
+    console.log(i);
+}
+*/
+
+
+
