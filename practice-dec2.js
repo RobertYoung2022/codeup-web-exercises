c334c16c3adf7f44f8896599cfd126488fe94de2
Robert Young <robertayoung2022@gmail.com>
2021-12-02 18:44:12 -0600

    updates to practice js file

--- practice-dec2.js
+++ practice-dec2.js
@@ -1,7 +1,10 @@
 "use strict";
 
 //Loops
-// -- WHILE --
+
+
+//           -- WHILE --
+
 //20 parking spots, 1 car driving around, ? how many times we'll have to drive around before finding a spot
 
 /*while(condition){
@@ -69,7 +72,10 @@ while(!parked){
     }
 }*/
 
-// -- DO WHILE --
+
+
+//      -- DO WHILE --
+
 //Executes the code before it checks then checks the conditional
 /*let i = 3;
 do{
@@ -78,26 +84,98 @@ do{
     console.log("i after i++: ", i)
 } while(i <= 15);*/
 
-// EXAMPLE - loop through numbers 0 - 50 by 2s
-let i = 0;
+//      EXAMPLE - loop through numbers 0 - 50 by 2s
+/*let i = 0;
 do{
     console.log("until we get to: ", i);
     i++;
     i++;
 //    or i = i + 2;
-} while(i <= 50);
-
+} while(i <= 50);*/
 
 //      After Lunch - Exercise
-let i = 0
+/*let i = 0
 while(i <= 10){
     console.log("While Loop:", i);
     i++;
-}
+}*/
 
-let r = 0
+/*let r = 0
 do{
     console.log("Do-While Loop:", r);
     r++;
-} while(r <= 10)
-// -- FOR --
+} while(r <= 10)*/
+
+// TODO: using prompt() and alert(), write a loop that will continue to ask for the correct password until the
+// correct password is given.
+
+//         My version of the prompt() and alert() with a WHILE loop exercise
+/*let password = "great";
+let something = prompt("What is the correct password?");
+let equal = false;
+while(!equal) {
+    if(password === something){
+        equal = true
+    } else{
+        something = prompt("What is the correct password?");
+    }
+}
+alert("You are Great");*/
+
+//          Instructor Example for using prompt() and alert() with a WHILE loop
+/*let correctPass = "secret";
+let input = prompt("What is the password?");
+while(input !== correctPass) {
+    alert("Wrong password, try again!")
+    input = prompt("Enter the password again?");
+}
+
+alert("Password Accepted");*/
+
+
+//      -- FOR loops --
+
+
+//          FOR loop structure
+/*for(1; 2; 3;) {
+    // code;
+}*/
+
+//1. create variable / starting point
+//2. condition / stopping point
+//3. times to run & display / how to iterate
+
+
+//          FOR loop EXAMPLE #1
+/*for(let i = 0; i <= 15; i++) {
+    console.log("Your favorite number is: ", i);
+}*/
+
+//          FOR loop EXAMPLE #1.2
+/*for(let x = 0; x <= 15; x+= 3) {
+    console.log("Your favorite number is: ", x);
+}*/
+
+//          FOR loop EXAMPLE #2
+/*let dozens = 0;
+// want 4 dozens
+
+for(let eggs = 0; eggs <= 48; eggs++) {
+    dozens = eggs / 12;
+    console.log("dozens: ", dozens);
+    console.log("i: ", eggs);
+}*/
+
+//         *** NESTED FOR loop ***
+/*for(let counter = 1; counter <=5; counter++) {
+    console.log("1st counter: " + counter);
+    for (let counterTwo = 5; counterTwo > 0; counterTwo--) {
+        console.log("countdown: " + counterTwo);
+    }
+}*/
+
+//          Example of FOR loop with an array
+/*let animals = ["dog","horse", "frog", "cat"];
+for(let x = 0; x < animals.length; x++) {
+    console.log("Your favorite number is: "+  animals[x]);
+}*/
