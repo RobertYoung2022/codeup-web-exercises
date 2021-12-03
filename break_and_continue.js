5580513bb7c331d735f99758359fed521a00c4c5
Robert Young <robertayoung2022@gmail.com>
2021-12-03 09:46:54 -0600

    Updates to break and continue file

--- break_and_continue.js
+++ break_and_continue.js
@@ -15,6 +15,22 @@ while(answer) {
     }
     console.log("okay good");
 }*/
+//Instructor answer to this
+/*let userNumber = 0;
+do{
+    userNumber = Number(prompt("Gimme an odd number between 1 - 50"));
+    //check to see if between 1 -50
+    if(userNumber < 1 || userNumber > 50) {
+        alert("Number out of bounds");
+    } else if (userNumber % 2 === 0) {
+        alert("Number is not odd");
+    } else if(isNaN(userNumber)) {
+        alert("That's not a number")
+    } else {
+        alert("You got it right!");
+        break;
+    }
+} while(true)*/
 
 //         Same solution minus the break
 /*while(answer) {
@@ -33,12 +49,12 @@ while(answer) {
 let askForOddNumber2 = prompt("Pick an odd number between 1 - 50");
 let answer2 = askForOddNumber2 % 2 == 0
 let middle = 1; middle <= 50; middle++
-/*for(let middle = 1; middle <= 50; middle++) {
+for(let middle = 1; middle <= 50; middle++) {
         if (answer2) {
             continue;
     }
     console.log("All the odd numbers " + answer2 + " minus yours lol.")
-}*/
+}
 
 /*for(middle of answer2){
 
@@ -49,5 +65,15 @@ let middle = 1; middle <= 50; middle++
     console.log("All the odd numbers " + answer2 + "minus yours lol.")
 }*/
 
+//      CORRECT ANSWER
+let userNumber2 = Number(prompt("Gimme an odd number between 1 - 50"));
+// console.log("Number to skip: " + userNumber2)
 
+for(let i = 1; i <=50; i += 2) {
+    if(i === userNumber2) {
+        console.log("Yikes: skipping number " + userNumber2);
+        continue;
+    }
+    console.log("Here is an odd number " + i);
+}
 
