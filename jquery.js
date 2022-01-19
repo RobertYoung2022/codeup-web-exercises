ebd02083a7717116b998bcfed0d61ac5a08072d0
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-01-19 12:29:53 -0600

    quiz day

---
+++ jquery.js
@@ -0,0 +1,10 @@
+// var array = ['John', 'Paul', 'George', 'Ringo'];
+//
+// array.splice(array.indexOf('Ringo'), 0, 'Yoko');
+//
+// console.log(array);
+
+
+band.shred = function() {
+    console.log('RAAAAAAAAAA!!!!');
+}
