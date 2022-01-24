76d346e39cd5e2288839dead5582a4e2a3904950
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-01-24 10:26:18 -0600

    created files for jquery faq

--- Javascript/exercise-jan24.js
+++ Javascript/exercise-jan24.js
@@ -8,3 +8,14 @@ $(".border").css("border" , "2px solid black");
 //change h1 to say "Happy Monday"
 $("h1").html("Happy Monday!");
 $("h1").html();
+
+// add a class of border to my h2
+
+$("#red").addClass("border"); // GETTER
+$("#red").removeClass("border"); // SETTER -->
+
+// .toggleClass()
+
+$("h1").click(function () { // this function is what is going to happen when h1 is clicked
+    $("h1").toggleClass("highlight"); // this is the action you want to happen after you click
+})
