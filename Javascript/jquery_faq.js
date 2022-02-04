3a6f4bce98c3ce226d36cd1ad8b82606b7324c25
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-02-04 11:17:17 -0600

    updating several files

--- Javascript/jquery_faq.js
+++ Javascript/jquery_faq.js
@@ -1,11 +1,26 @@
-// $("dd").addClass("invisible");
+// . ATTRIBUTES AND CSS
 
-$("button").click(function () {
+$("#button").click(function () {
    if ($("dd").css("visibility") == "hidden")
        $("dd").toggleClass("invisible" , "visible");
    else $("dd").toggleClass("invisible" , "hidden");
 });
 
 $("dt").click(function () {
-    $(this).toggleClass("dt").css({"background-color" : "lightblue"})
-})
+    $(this).toggleClass("dt").css({"background-color" : "lightblue"});
+});
+
+//  TRAVERSING
+
+//CHANGE BACKGROUND TO YELLOW FOR LAST CHILD OF UNORDERED LIST
+$("#facts-button").click(function () {
+    $("li:last-child").css({background : "yellow"})
+});
+
+$("h3").click(function () {
+    $(this).next().children().css("font-weight" , "bold")
+});
+
+$("ul").click(function () {
+   $(this).children("li:first-child").toggleClass("blue")
+});
