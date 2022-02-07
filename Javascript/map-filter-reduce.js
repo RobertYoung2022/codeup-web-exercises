5e6f098d5a6e5fd401ba062addf2d591fcf40dcf
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-02-07 16:51:34 -0600

    completed map-filter-reduce assignment

--- Javascript/map-filter-reduce.js
+++ Javascript/map-filter-reduce.js
@@ -1,39 +1,99 @@
+"use strict";
 
-const users = [
-    {
-        id: 1,
-        name: 'ryan',
-        email: 'ryan@codeup.com',
-        languages: ['clojure', 'javascript'],
-        yearsOfExperience: 5
-    },
-    {
-        id: 2,
-        name: 'luis',
-        email: 'luis@codeup.com',
-        languages: ['java', 'scala', 'php'],
-        yearsOfExperience: 6
-    },
-    {
-        id: 3,
-        name: 'zach',
-        email: 'zach@codeup.com',
-        languages: ['javascript', 'bash'],
-        yearsOfExperience: 7
-    },
-    {
-        id: 4,
-        name: 'fernando',
-        email: 'fernando@codeup.com',
-        languages: ['java', 'php', 'sql'],
-        yearsOfExperience: 8
-    },
-    {
-        id: 5,
-        name: 'justin',
-        email: 'justin@codeup.com',
-        languages: ['html', 'css', 'javascript', 'php'],
-        yearsOfExperience: 9
-    }
-];
+(function () {
+    const users = [
+        {
+            id: 1,
+            name: 'ryan',
+            email: 'ryan@codeup.com',
+            languages: ['clojure', 'javascript'],
+            yearsOfExperience: 5
+        },
+        {
+            id: 2,
+            name: 'luis',
+            email: 'luis@codeup.com',
+            languages: ['java', 'scala', 'php'],
+            yearsOfExperience: 6
+        },
+        {
+            id: 3,
+            name: 'zach',
+            email: 'zach@codeup.com',
+            languages: ['javascript', 'bash'],
+            yearsOfExperience: 7
+        },
+        {
+            id: 4,
+            name: 'fernando',
+            email: 'fernando@codeup.com',
+            languages: ['java', 'php', 'sql'],
+            yearsOfExperience: 8
+        },
+        {
+            id: 5,
+            name: 'justin',
+            email: 'justin@codeup.com',
+            languages: ['html', 'css', 'javascript', 'php'],
+            yearsOfExperience: 9
+        }
+    ];
 
+    let threeLanguages = users.filter(user => user.languages.length >= 3);
+    console.log(threeLanguages);
+
+    let emailString = users.map(user => user.email);
+    console.log(emailString);
+
+    let totalYears = users.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue.yearsOfExperience;
+    }, 0);
+    console.log(totalYears);
+
+    // getting the average of years of experiences (BONUS)
+    let avgExp = totalYears / users.length;
+    console.log(avgExp);
+
+
+    // let longestEmail = emailString.reduce((previousValue, currentValue) => {
+    // if(previousValue.length > currentValue.length) {
+    //     return previousValue;
+    // } else {
+    //     return currentValue;
+    // }
+
+    let longestEmail = emailString.reduce((previousValue, currentValue) => {
+        return previousValue.length > currentValue.length ? previousValue : currentValue;
+    });
+    console.log(longestEmail);
+
+    let instructors = users.reduce((finalString, nextUser, index) => {
+        if(index === users.length -1) {
+            return finalString + nextUser.name + ".";
+        } else {
+            return finalString + nextUser.name + ",";
+        }
+    }, "Your instructors are: ");
+
+    console.log(instructors);
+
+// BONUS
+    let uniqueLanguages = users.reduce((langs, currentVal) => {
+        currentVal.languages.forEach(language => {
+            if(!langs.includes(language)) {
+                langs.push(language);
+            }
+        });
+        return langs
+    }, []);
+    console.log(uniqueLanguages);
+})();
+
+
+// 2.
+
+
+// 3.
+
+
+// 4.
