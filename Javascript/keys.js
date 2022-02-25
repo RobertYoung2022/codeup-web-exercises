7cce246539b1164258e70430538c3a4bd3f29009
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-02-25 11:35:58 -0600

    completed promises assignment

---
+++ Javascript/keys.js
@@ -0,0 +1,1 @@
+let url = "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";
--- Javascript/promises.js
+++ Javascript/promises.js
@@ -1,3 +1,31 @@
 "use strict";
 
-fetch(url, {headers: {'Authorization': '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}})
+
+$(function () {
+    function gitLastCommit(user) {
+        return fetch(`https://api.github.com/users/${user}/events/public`, {
+            headers: {
+                'authorization': 'GITHUB'
+            }
+        }).then(response => response.json())
+            .then(users => {
+                console.log(users[0]);
+                let lastCommit = `<p>${users[0].actor.login} Last Commit: ${users[0].created_at}</p>`
+                $(".container").html(lastCommit)
+            }).catch(err => console.log(err))
+    }
+
+
+    console.log(gitLastCommit("RobertYoung2022"));
+
+    function wait(num) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(`${num}`)
+            }, num)
+        })
+    }
+
+    wait(3000).then((num) => $(".container").append((`<p>You'll have to push faster! ${num/2000} seconds</p>`)))
+
+})();
